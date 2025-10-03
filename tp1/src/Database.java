5fb902cb36855f9bb6a7aceab8b66fcc8ac06524
s3nkumega <75550482+s3nkumega@users.noreply.github.com>
2025-10-03 12:49:15 -0400

    correction

---
+++ tp1/src/Database.java
@@ -0,0 +1,3 @@
+public interface Database {
+    void save(String data);
+}
---
+++ tp1/src/MySQLDatabase.java
@@ -0,0 +1,3 @@
+class MySQLDatabase implements Database {
+    public void save(String data) { System.out.println("MySQL: " + data); }
+}
